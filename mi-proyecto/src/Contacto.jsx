ce8a0a990084c20109fbee045089f7de2ef25c1b
Julián Luque <julianluqueeig@gmail.com>
2026-04-27 21:17:41 +0200

    fix: restaurar archivos vacios del ejercicio 1

---
+++ mi-proyecto/src/Contacto.jsx
@@ -0,0 +1,8 @@
+export default function Contacto() {
+  return (
+    <div>
+      <h1>Contacto</h1>
+      <p>Aquí irá el componente del Formulario de contacto.</p>
+    </div>
+  );
+}
---
+++ mi-proyecto/src/Inicio.jsx
@@ -0,0 +1,8 @@
+export default function Inicio() {
+  return (
+    <div>
+      <h1>Inicio</h1>
+      <p>Bienvenido a nuestra aplicación. Este es el proyecto desarrollado para la práctica de Alternancia.</p>
+    </div>
+  );
+}
